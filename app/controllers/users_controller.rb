2b733b7796fc3b342c326222b6f5b8e3dcb325ca
homarinn <homarinn1201@gmail.com>
2019-02-11 05:30:23 +0000

    user login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:show]
+  
   def show
     @user = User.find(params[:id])
   end
@@ -22,6 +24,6 @@ class UsersController < ApplicationController
   private
   
   def user_params
-    params.require(:users).parmit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
